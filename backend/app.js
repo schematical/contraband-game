72a7c7bc1755de3242a6075acf80d42f4c34f64f
Matt <mlea@schematical.com>
2019-04-18 11:38:48 -0500

    Just getting a base app setup so we can add a build pipeline. NO REAL GAME LOGIC OR GAME ASSETS

--- backend/app.js
+++ backend/app.js
@@ -3,5 +3,5 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => res.send('Schematical Games!'));
-
+app.get('/heartbeat', (req, res) => res.send('I am alive'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
